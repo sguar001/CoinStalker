525b160e17aa28de011bbcb8b521252bf5eab5ec
Jeremiah Griffin <jgrif007@ucr.edu>
2018-11-12 21:54:31 -0800

    Add exact option to currentPriceWidget

--- src/coinstalker/lib/price_widget.dart
+++ src/coinstalker/lib/price_widget.dart
@@ -6,13 +6,21 @@ import 'cryptocompare.dart';
 import 'database.dart';
 import 'session.dart';
 
-Widget currentPriceWidget(String fromSymbol) => streamWidget(
-    stream:
-        Profile.buildStream(Session().profileRef).map((x) => x.displaySymbol),
-    waitBuilder: emptyWaitBuilder,
-    builder: (context, toSymbol) => futureWidget(
-        future: CryptoCompare().price(fromSymbol, toSymbol),
-        waitBuilder: emptyWaitBuilder,
-        builder: (context, price) => Text(NumberFormat.simpleCurrency(
-                locale: Intl.systemLocale, name: toSymbol)
-            .format(price))));
+/// Builds a widget containing the quote price of the given symbol in the user's
+/// chosen display symbol
+Widget currentPriceWidget(String fromSymbol, {bool exact = false}) =>
+    streamWidget(
+      stream:
+          Profile.buildStream(Session().profileRef).map((x) => x.displaySymbol),
+      waitBuilder: emptyWaitBuilder,
+      builder: (context, toSymbol) => futureWidget(
+            future: CryptoCompare().price(fromSymbol, toSymbol),
+            waitBuilder: emptyWaitBuilder,
+            builder: (context, price) {
+              final formatted = NumberFormat.simpleCurrency(
+                      locale: Intl.systemLocale, name: toSymbol)
+                  .format(price);
+              return Text(exact ? '$formatted ($price)' : formatted);
+            },
+          ),
+    );
